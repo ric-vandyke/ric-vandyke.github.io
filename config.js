eae7dd8e34025f8f86d1f0d40d0aefce8d881b0d
Eric VanDyke <ericvandyke@Erics-MacBook-Air.local>
2025-12-30 10:52:32 -0700

    Changes from 12.30.2025

---
+++ config.js
@@ -0,0 +1,15 @@
+window.siteConfig = {
+  links: {
+    home: 'index.html',
+    projects: {
+      one: 'projects/one.html',
+      two: 'projects/two.html',
+      three: 'projects/three.html',
+    },
+    socials: {
+      linkedin: 'https://www.linkedin.com/in/hireericvandyke/',
+      x: 'https://x.com/controlsbarista',
+      instagram: 'https://www.instagram.com/ricvandyke/',
+    },
+  },
+};
---
+++ init-links.js
@@ -0,0 +1,34 @@
+(function () {
+  if (!window.siteConfig || !window.siteConfig.links) return;
+
+  var links = window.siteConfig.links;
+  var inProjectsDir = window.location.pathname.indexOf('/projects/') !== -1;
+  var rootPrefix = inProjectsDir ? '..' : '.';
+
+  var withPrefix = function (path) {
+    if (!path) return '';
+    if (/^https?:\/\//i.test(path) || path.indexOf('//') === 0) {
+      return path;
+    }
+    return (rootPrefix + '/' + path).replace(/\/+/g, '/');
+  };
+
+  var linkMap = {
+    'home': links.home,
+    'projects.one': links.projects && links.projects.one,
+    'projects.two': links.projects && links.projects.two,
+    'projects.three': links.projects && links.projects.three,
+    'socials.linkedin': links.socials && links.socials.linkedin,
+    'socials.x': links.socials && links.socials.x,
+    'socials.instagram': links.socials && links.socials.instagram,
+  };
+
+  var anchors = document.querySelectorAll('[data-config-link]');
+  anchors.forEach(function (anchor) {
+    var key = anchor.getAttribute('data-config-link');
+    var path = linkMap[key];
+    if (path) {
+      anchor.setAttribute('href', withPrefix(path));
+    }
+  });
+})();
